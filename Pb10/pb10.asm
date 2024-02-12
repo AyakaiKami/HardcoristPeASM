19bfe1ad5ceab430a2f2afb225e497323e7b263b
Stefan <stefancristea27@gmail.com>
2024-02-12 16:44:14 +0200

    kkok

--- Pb10/pb10.asm
+++ Pb10/pb10.asm
@@ -6,9 +6,24 @@ section .text
     global _start
 
 _start:
-
-
+    ;https://projecteuler.net/problem=10
+    ;sum of primes
+    mov rax,10
+    call _print
     ;exit call
     mov rax,60
     mov rdi,0
     syscall
+
+
+_print:
+    mov rcx,digitSpace
+    mov rbx,10
+    mov [rcx],rbx
+    inc rcx 
+    mov [digitSpacePos],rcx
+
+    _stringMakeLoop:
+        mov rcx,[digitSpacePos]
+        xor rdx,rdx
+        div rbx
