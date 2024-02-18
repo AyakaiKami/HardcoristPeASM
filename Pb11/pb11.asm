3ec483d21b4a9ad6400442faf4c9babbbc4cb540
Stefan <stefancristea27@gmail.com>
2024-02-18 19:53:09 +0200

    dsadssada

--- Pb11/pb11.asm
+++ Pb11/pb11.asm
@@ -19,25 +19,33 @@ _print:
     mov rcx,digitSpace
     mov rbx,10
     mov [rcx],bl
+    inc rcx
     mov [digitSpacePos],rcx
 
     _stringMakeLoop:
         xor rdx,rdx
         div rbx
         add rdx,48
+        mov rcx,[digitSpacePos]
         mov [rcx],dl
 
-        mov rcx,[digitSpacePos]
         inc rcx
         mov [digitSpacePos],rcx
         cmp rax,0
         jne _stringMakeLoop
 
     _printString:
-        mov rax,0
+        mov rcx,[digitSpacePos]
+        mov rax,1
         mov rdi,1
         mov rdx,1
         mov rsi,rcx
         syscall
+
+        mov rcx,[digitSpacePos]
+        dec rcx
+        mov [digitSpacePos],rcx
+        cmp rcx,digitSpace
+        jge _printString
 
 ret
